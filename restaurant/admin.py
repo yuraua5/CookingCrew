eac1fc643784813a9eaa41a095a3f31e5b192d83
Yura <yuraua5@gmail.com>
2023-05-08 17:08:22 +0300

    Registered models on the admin site

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from restaurant.models import DishType, Dish, Cook
+
+admin.site.unregister(Group)
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(DishType)
+admin.site.register(Dish)
