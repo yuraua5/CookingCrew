d0de7adf7a23081e158b75bd819f6f8f7eeed99e
Yura <yuraua5@gmail.com>
2023-05-08 22:27:47 +0300

    add url in urls.py, create view and empty templates

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,84 @@
 from django.urls import path
 
-from restaurant.views import index
+from restaurant.views import (
+    index,
+    DishListView,
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+    CookListView,
+    CookCreateView,
+    CookUpdateView,
+    CookDeleteView
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create"
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete"
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list"
+    ),
+    path(
+        "dishes/create/",
+        DishCreateView.as_view(),
+        name="dish-create"
+    ),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update"
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete"
+    ),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list"
+    ),
+    path(
+        "cooks/create/",
+        CookCreateView.as_view(),
+        name="cook-create"
+    ),
+    path(
+        "cooks/<int:pk>/update/",
+        CookUpdateView.as_view(),
+        name="cook-update"
+    ),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete"
+    ),
+
+
 ]
 app_name = "restaurant"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views import generic
 
-from restaurant.models import DishType, Dish
+from restaurant.models import DishType, Dish, Cook
 
 
 def index(request):
@@ -15,3 +16,61 @@ def index(request):
         "num_dishes": num_dishes,
     }
     return render(request, "restaurant/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "restaurant/dish_type_list.html"
+
+
+class DishTypeCreateView(generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    template_name = "restaurant/dish_type_form.html"
+
+
+class DishTypeUpdateView(generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    template_name = "restaurant/dish_type_form.html"
+
+
+class DishTypeDeleteView(generic.DeleteView):
+    model = DishType
+    template_name = "restaurant/dish_type_confirm_delete.html"
+
+
+class DishListView(generic.ListView):
+    model = Dish
+
+
+class DishCreateView(generic.CreateView):
+    model = Dish
+    fields = "__all__"
+
+
+class DishUpdateView(generic.UpdateView):
+    model = Dish
+    fields = "__all__"
+
+
+class DishDeleteView(generic.DeleteView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
+
+
+class CookCreateView(generic.CreateView):
+    model = Cook
+    fields = "__all__"
+
+
+class CookUpdateView(generic.UpdateView):
+    model = Cook
+    fields = "__all__"
+
+
+class CookDeleteView(generic.DeleteView):
+    model = Cook
