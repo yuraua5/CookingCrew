83fcc70abd8fc10ecf01b9f0e3041d7d5fde19c1
Yura <yuraua5@gmail.com>
2023-05-08 17:37:39 +0300

    Configured urls.py, add index view, created base.html, index.html in templates

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from restaurant.models import DishType, Dish
+
+
+def index(request):
+    num_dish_types = DishType.objects.count()
+    num_cooks = get_user_model().objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+    }
+    return render(request, "restaurant/index.html", context=context)
