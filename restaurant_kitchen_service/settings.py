181ddff960b5783890005739af91959d5e466246
Yura <yuraua5@gmail.com>
2023-05-10 20:09:10 +0300

    Add footer, navigation, scripts and update base, index templates, add assets, context_processors.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "restaurant.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -135,3 +136,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "restaurant.Cook"
+
+ASSETS_ROOT = "/static/assets"
