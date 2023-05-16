17638c64016764052ef042229e9189f97aafafaf
Yura <yuraua5@gmail.com>
2023-05-16 22:03:45 +0300

    Create DishesDetailView and CooksDetailView and add success_urls in views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from restaurant.models import DishType, Dish, Cook
@@ -29,17 +30,20 @@ class DishTypeCreateView(generic.CreateView):
     model = DishType
     fields = "__all__"
     template_name = "restaurant/dish_type_form.html"
+    success_url = reverse_lazy("restaurant:dish-type-list")
 
 
 class DishTypeUpdateView(generic.UpdateView):
     model = DishType
     fields = "__all__"
     template_name = "restaurant/dish_type_form.html"
+    success_url = reverse_lazy("restaurant:dish-type-list")
 
 
 class DishTypeDeleteView(generic.DeleteView):
     model = DishType
     template_name = "restaurant/dish_type_confirm_delete.html"
+    success_url = reverse_lazy("restaurant:dish-type-list")
 
 
 class DishListView(generic.ListView):
@@ -49,11 +53,17 @@ class DishListView(generic.ListView):
 class DishCreateView(generic.CreateView):
     model = Dish
     fields = "__all__"
+    success_url = reverse_lazy("restaurant:dish-list")
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
 
 
 class DishUpdateView(generic.UpdateView):
     model = Dish
     fields = "__all__"
+    success_url = reverse_lazy("restaurant:dish-list")
 
 
 class DishDeleteView(generic.DeleteView):
@@ -67,11 +77,18 @@ class CookListView(generic.ListView):
 class CookCreateView(generic.CreateView):
     model = Cook
     fields = "__all__"
+    success_url = reverse_lazy("restaurant:cook-list")
+
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+    template_name = "restaurant/cook_detail.html"
 
 
 class CookUpdateView(generic.UpdateView):
     model = Cook
     fields = "__all__"
+    success_url = reverse_lazy("restaurant:cook-list")
 
 
 class CookDeleteView(generic.DeleteView):
