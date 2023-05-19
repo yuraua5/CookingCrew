a8157c246a838c4830f83360a0b4081ca75e7b89
Yura <yuraua5@gmail.com>
2023-05-19 17:56:38 +0300

    Save secret key in .env, add .env to .gitignore

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -12,16 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-d_=b(n)p54-=g5vs-5)8o6w-w_po#sn+7*552#-r0s30h4g^sv"
-)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
