33134c70fa9b26b9a9a19a0abf98f4381b257ae2
Yura <yuraua5@gmail.com>
2023-05-19 16:36:14 +0300

    fix flake8 errors

--- restaurant/models.py
+++ restaurant/models.py
@@ -29,7 +29,10 @@ class Dish(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="dishes"
+    )
 
     class Meta:
         ordering = ["name"]
