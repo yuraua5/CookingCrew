65e2f7403782d11271309d721f7491eec00c3cc2
Yura <yuraua5@gmail.com>
2023-05-20 12:33:50 +0300

    Update settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -27,9 +27,8 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
